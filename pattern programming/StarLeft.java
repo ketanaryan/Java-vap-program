013d719907a83c528bf564c4ed10d6d42edbc55b
Aryan yadav <iamketan3@gmail.com>
2026-04-10 11:08:04 +0530

    Pattern Programming

---
+++ pattern programming/StarLeft.java
@@ -0,0 +1,27 @@
+
+// import java.util.Scanner;
+public class StarLeft {
+    public static void main(String[] args){
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter number starts");
+        // int num = sc.nextInt();
+
+        for(int i =1;i<=5;i++){
+            for (int j = 1 ;j<=5;j++){
+                if(i==j||i==1||j==1){
+                    System.out.print("* ");
+                }
+                else{
+
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+
+           
+        }
+        }
+
+    }
+    
+
